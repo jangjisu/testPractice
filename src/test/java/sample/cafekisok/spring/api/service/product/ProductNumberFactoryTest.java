a8e9088c7e2ae46582c11280635f7b571740ce90
jangjisu <jsjangdv@gmail.com>
2024-11-25 22:14:33 +0900

    Private 메소드 테스트를 위한 객체 분

---
+++ src/test/java/sample/cafekisok/spring/api/service/product/ProductNumberFactoryTest.java
@@ -0,0 +1,19 @@
+package sample.cafekisok.spring.api.service.product;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductNumberFactoryTest {
+    @DisplayName("")
+    @Test
+    void test() {
+        // given
+
+        // when
+
+        // then
+    }
+
+}
